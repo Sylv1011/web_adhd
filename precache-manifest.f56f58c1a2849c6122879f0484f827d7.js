104d63488f86f616ef34dc40b402e2b25c431dbb
Sylvv <sylviading1011@gmail.com>
2020-12-11 07:40:11 -0800

    Updates

---
+++ precache-manifest.f56f58c1a2849c6122879f0484f827d7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0499bc37c45498ec2f7b132f8dace6a8",
+    "url": "/web_adhd/index.html"
+  },
+  {
+    "revision": "1abcb4c31894f81d3f4d",
+    "url": "/web_adhd/static/css/main.b0d2e4c2.chunk.css"
+  },
+  {
+    "revision": "36fdeca5c2c5ee8c0470",
+    "url": "/web_adhd/static/js/2.fcf917ba.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/web_adhd/static/js/2.fcf917ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abcb4c31894f81d3f4d",
+    "url": "/web_adhd/static/js/main.fe98d298.chunk.js"
+  },
+  {
+    "revision": "e86be64443937121cde1",
+    "url": "/web_adhd/static/js/runtime-main.97785a29.js"
+  },
+  {
+    "revision": "205bf35ab7d1d6c17cc9ad873f819a50",
+    "url": "/web_adhd/static/media/Venn.205bf35a.png"
+  },
+  {
+    "revision": "2a3eb4fdd2c5411a9bb137b1e38bcaf3",
+    "url": "/web_adhd/static/media/adderall.2a3eb4fd.jpg"
+  },
+  {
+    "revision": "4e3983c902e177776659301781e7fdc1",
+    "url": "/web_adhd/static/media/adhdBrain.4e3983c9.jpg"
+  },
+  {
+    "revision": "12ebf26ea2f354694e9d9edc9c970823",
+    "url": "/web_adhd/static/media/adhdWoman.12ebf26e.png"
+  },
+  {
+    "revision": "61982907bf0121a982af3435ce1e0948",
+    "url": "/web_adhd/static/media/adhd_girl.61982907.jpg"
+  },
+  {
+    "revision": "ee3d6c386eb51a3dc743603128c99451",
+    "url": "/web_adhd/static/media/cbt.ee3d6c38.png"
+  },
+  {
+    "revision": "1d2189b51bc003e82b0a27de3a96b76a",
+    "url": "/web_adhd/static/media/cloud1.1d2189b5.png"
+  },
+  {
+    "revision": "88ecc572a494fe9c426bd5f92fc845c9",
+    "url": "/web_adhd/static/media/cloud2.88ecc572.png"
+  },
+  {
+    "revision": "9cfee12d2dd042a15bba09e82f745815",
+    "url": "/web_adhd/static/media/cloud3.9cfee12d.png"
+  },
+  {
+    "revision": "23beb7461ee9d34efb25787fc8cfc147",
+    "url": "/web_adhd/static/media/cloud4.23beb746.png"
+  },
+  {
+    "revision": "b54e05127137d79c3b85a11a48db931c",
+    "url": "/web_adhd/static/media/cloud5.b54e0512.png"
+  },
+  {
+    "revision": "33cfcfa1ee46346cb3140e7b5a2cbd66",
+    "url": "/web_adhd/static/media/cryingMan.33cfcfa1.png"
+  },
+  {
+    "revision": "9daab62dd421523035767eede5891153",
+    "url": "/web_adhd/static/media/dopamine.9daab62d.png"
+  },
+  {
+    "revision": "a9cecac50c1854aa24d884b0cb170050",
+    "url": "/web_adhd/static/media/family.a9cecac5.png"
+  },
+  {
+    "revision": "f0fcb9d13613f03cd77fa888907ac64e",
+    "url": "/web_adhd/static/media/group.f0fcb9d1.png"
+  },
+  {
+    "revision": "8436568907df16923432fda49fb7b8ed",
+    "url": "/web_adhd/static/media/main_1.84365689.png"
+  },
+  {
+    "revision": "e3e2faa05c3e81f05727c415b46f3cad",
+    "url": "/web_adhd/static/media/s3Title.e3e2faa0.png"
+  },
+  {
+    "revision": "d4cabf4062f72946034f10c3cf58bba9",
+    "url": "/web_adhd/static/media/strattera.d4cabf40.jpg"
+  },
+  {
+    "revision": "92ebc5ba089704d308dc339991a01b9a",
+    "url": "/web_adhd/static/media/type1.92ebc5ba.png"
+  },
+  {
+    "revision": "ee4a2466049ddf038e3669562e99c6b4",
+    "url": "/web_adhd/static/media/type2.ee4a2466.png"
+  },
+  {
+    "revision": "d1a690a2b9fecd6f4aa11ce7d2273e80",
+    "url": "/web_adhd/static/media/wellbutrin.d1a690a2.png"
+  }
+]);
